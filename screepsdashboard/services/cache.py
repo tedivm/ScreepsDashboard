c85d98d77a57690740afa0d80e2ebdf15a8fb6f6
Robert Hafner <tedivm@tedivm.com>
2017-12-05 00:09:56 -0800

    change cache type to dbm from file

--- screepsdashboard/services/cache.py
+++ screepsdashboard/services/cache.py
@@ -2,10 +2,10 @@ from beaker.cache import CacheManager
 from beaker.util import parse_cache_config_options
 
 from screepsdashboard import app
-cache_root = app.config.get('CACHE_ROOT', "/tmp/screeps_dashboard")
+cache_root = app.config.get('CACHE_ROOT', "/tmp/screepsdash")
 
 cache_opts = {
-    'cache.type': 'file',
+    'cache.type': 'dbm',
     'cache.data_dir': cache_root + '/data',
     'cache.expire': 3600,
     'cache.lock_dir': cache_root + '/lock'
